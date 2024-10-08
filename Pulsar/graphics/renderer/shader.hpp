81d965dbdc42d0cd28e7e0ba5e5c6f170aae8314
Marek <98259329+Pulsarnixx@users.noreply.github.com>
2024-10-08 19:01:51 +0200

    Shader OOP, example app code shortening (#3)

--- Pulsar/graphics/renderer/shader.hpp
+++ Pulsar/graphics/renderer/shader.hpp
@@ -2,9 +2,35 @@
 
 #include <string>
 
-struct ShaderSource{
-    std::string s_vertexSource;
-    std::string s_fragmentSource;
-};
+//Forward dec.
+struct ShaderSource;
 
-ShaderSource ReadGLSLFromFile(const std::string& filepath);
+/*
+    Class represents shader program
+*/
+class Shader{
+
+public:
+        //Constr, Destr.
+        Shader(const std::string& filepath);
+        ~Shader();
+
+        //Utilities
+        void Bind() const;
+        void UnBind() const;
+
+private:
+    ShaderSource ReadGLSLFromFile(const std::string& filepath);
+    unsigned int CompileShader(unsigned int type, const std::string& source);
+    unsigned int GenerateShaderProgram(const std::string& vertexShader, const std::string& fragmentShader);
+    //PrintSourceFile()
+    //GetId()
+    
+private:
+    unsigned int m_ID;
+    std::string m_FilePath; //Debugging purpose
+
+
+
+
+};
